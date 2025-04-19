9c9619ff540f86900be3f8f1bcb9665ac9b7ef5a
jhermosillo374 <jonathan.hermosillo@csuglobal.edu>
2025-04-19 08:56:25 -0600

    Update SyntaxCorrection2.cpp

--- SyntaxCorrection2.cpp
+++ SyntaxCorrection2.cpp
@@ -5,19 +5,19 @@
  *      Author: jonathan
  */
 #include <iostream>
-#include <conio.h> // This is Windows-specific; avoid in portable code
+#include <conio.h> 
 
 using namespace std;
 
 int main() {
-    double myMoney = 1000.50; // this should be printed out
+    double myMoney = 1000.50; // This amount will be printed out
 
-    // Standard Output Statements
+    // Output Statements
     cout << "Please be sure to correct all syntax errors in this program" << endl;
     cout << "I have corrected all errors for this program." << endl;
     cout << "The total amount of money available is = " << myMoney << endl;
 
-    // Wait for user to press a key (only works in some environments)
+    // Wait for user to press a key 
     _getch(); // or use cin.get() if you want cross-platform compatibility
 
     return 0;
